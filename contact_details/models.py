61a45056dc5f315ec3df8af21dec4ef59ebb4ed8
Prithviraju Venkataraman <prithvirajuvenkataraman@gmail.com>
2023-12-11 17:22:44 -0800

    Last edit

--- contact_details/models.py
+++ contact_details/models.py
@@ -1,9 +1,13 @@
+# contact_details/models.py
 from django.db import models
 
 class Contact(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=100)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"{self.name} "
+        return self.name
+
+    class Meta:
+        app_label = 'contact_details'  
