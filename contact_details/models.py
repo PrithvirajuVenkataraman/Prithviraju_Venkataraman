75bc52c746e4ce853f663e398ba70fe54ab6b377
Prithviraju Venkataraman <prithvirajuvenkataraman@gmail.com>
2023-12-11 16:29:43 -0800

    Edit 2

--- contact_details/models.py
+++ contact_details/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Contact(models.Model):
-    Name = models.CharField(max_length=100)
+    name = models.CharField(max_length=150)
     email = models.EmailField()
     phone_number = models.CharField(max_length=15)
 
     def __str__(self):
-        return f"{self.Name} "
+        return f"{self.name} "
