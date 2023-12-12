80d7987c5f4b896d3f7ed797ddb988e5c4ac0d30
Prithviraju Venkataraman <prithvirajuvenkataraman@gmail.com>
2023-12-11 17:24:21 -0800

    Last edit

--- contact_details/forms.py
+++ contact_details/forms.py
@@ -1,7 +1,8 @@
+# contact_details/forms.py
 from django import forms
 from .models import Contact
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
-        fields = ['Name', 'email', 'phone_number']
+        fields = ['name', 'email', 'phone_number']
