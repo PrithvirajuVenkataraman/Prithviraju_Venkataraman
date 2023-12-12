21eeed582e504deabb7bec5da9b092c48132a516
Prithviraju Venkataraman <prithvirajuvenkataraman@gmail.com>
2023-12-11 16:33:18 -0800

    edit 3

--- contact_details/views.py
+++ contact_details/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Contact
+from .forms import ContactForm
 
-# Create your views here.
+def contact_list(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contact_details/contact_list.html', {'contacts': contacts})
+
+def contact_detail(request, pk):
+    contact = get_object_or_404(Contact, pk=pk)
+    return render(request, 'contact_details/contact_detail.html', {'contact': contact})
+
+def contact_create(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact_detail', pk=contact.pk)
+    else:
+        form = ContactForm()
+    return render(request, 'contact_details/contact_form.html', {'form': form})
+
